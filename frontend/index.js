c6e48e45f7c78a0e34fe6e1887538ff25862639e
Bhuvan <bhuvanasairam2005@gmail.com>
2025-07-14 06:14:31 +0530

    login form integration with backend

--- frontend/index.js
+++ frontend/index.js
@@ -1,17 +1,38 @@
-let login = document.getElementById("login-button");
-
-login.addEventListener("click", () => {
+let login = document.getElementById("loginForm");
+login.addEventListener("submit",async function(e) {
+  e.preventDefault();
   let username = document.getElementById("username").value;
   let role = document.getElementById("Role").value;
   let password = document.getElementById("password").value;
-  let warning = document.getElementById("warning").value;
-   
-  if(!username || !role || !password || !warning){
+  let warning = document.getElementById("warning");
+  if(!username || !role || !password){
+    warning.classList.remove("hidden");
     warning.innerText = "Enter all the details to login";
+    return;
   }
   else{
+    warning.innerText = "";
     try{
-      fetch("http://localhost:5000/api/auth/login")
+      const response = await fetch("http://localhost:5000/api/auth/login",{
+        method:"POST",
+        headers:{
+          "content-Type":"application/json"
+        },
+        body:JSON.stringify({username,password,role}),
+      });
+      const data = await response.json();
+      console.log(data);
+      if(response.ok){
+        if(role=="admin"){
+          window.location.href="adminDashBoard.html";
+        }
+        else if(role=="siteLead"){
+          window.location.href="siteLeadDashBoard.html";
+        }
+        else{
+          window.location.href="workerDashboard.html";
+        }
+      }
     }
     catch{
       alert("Cannot fetch data");
